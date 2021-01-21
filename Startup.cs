72adbee3b63f13eb2b9246a8ee10e74ccf6fec7f
Max <32428314+benkenobi3@users.noreply.github.com>
2021-01-21 18:49:05 +0300

    change basic options

--- Startup.cs
+++ Startup.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace asp.net_chat
 {
@@ -27,29 +27,52 @@ namespace asp.net_chat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "asp.net_chat", Version = "v1"});
+
+            services.AddSignalR(options => {
+                options.KeepAliveInterval = TimeSpan.FromSeconds(5);
+                options.ClientTimeoutInterval = TimeSpan.FromDays(365);
+                options.MaximumReceiveMessageSize = 4096;
+                options.StreamBufferCapacity = 4096;
             });
+            services.AddCors();
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Latest).AddNewtonsoftJson();
+            services.AddMemoryCache();
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "asp.net_chat v1"));
             }
+
+            app.UseCors(builder =>
+                builder
+                    .WithOrigins("http://localhost:8080")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+            );
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+            
+            app.UseStaticFiles();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(routes => {
+                routes.MapControllers();
+                routes.MapHub<ChatHub>("/chathub");
+            });
+
+            app.UseMvc();
         }
     }
 }
